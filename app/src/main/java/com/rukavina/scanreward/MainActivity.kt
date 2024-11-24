a76165c52ec4bedab199650a34879ce07dfc3e57
rukavina-git <ivankarlo.rukavina@gmail.com>
2024-11-24 08:01:18 +0100

    Add bottom navigation bar

--- app/src/main/java/com/rukavina/scanreward/MainActivity.kt
+++ app/src/main/java/com/rukavina/scanreward/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
 import com.google.firebase.perf.FirebasePerformance
+import com.rukavina.scanreward.navigation.BottomNavigationBar
 import com.rukavina.scanreward.navigation.NavigationGraph
 import com.rukavina.scanreward.ui.theme.ScanRewardTheme
 
@@ -19,30 +19,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
         setContent {
-            val navController = rememberNavController()
-            NavigationGraph(navController = navController)
+            ScanRewardTheme {
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = { BottomNavigationBar(navController = navController) }
+                ) { innerPadding ->
+                    NavigationGraph(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
-        //throw RuntimeException("Test Crash")
 
         val trace = FirebasePerformance.getInstance().newTrace("Log.d speed")
         trace.start()
         Log.d("Test", "testing performance")
         trace.stop()
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScanRewardTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/rukavina/scanreward/navigation/BottomNavigationBar.kt
@@ -0,0 +1,44 @@
+package com.rukavina.scanreward.navigation
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        Screen.Home,
+        Screen.Journey,
+        Screen.Rewards,
+        Screen.Profile
+    )
+
+    NavigationBar(
+        containerColor = Color.White,
+        contentColor = Color.Black
+    ) {
+        val navBackStackEntry = navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry.value?.destination?.route
+
+        items.forEach { screen ->
+            NavigationBarItem(
+                icon = { /* Add an icon here if desired */ },
+                label = { Text(screen.route.replaceFirstChar { it.uppercaseChar() }) },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    if (currentRoute != screen.route) {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
